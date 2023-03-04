131bbc67b050ca2f0ed56af573c85dcb2955b585
cipiripi13 <krstickatarina1990@gmail>
2023-03-04 15:56:46 +0100

    adding components in src folder

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () =>{
+    return(
+        <header>
+            <p className="header">Header</p>
+        </header>
+    )
+}
---
+++ src/components/MainContent.js
@@ -0,0 +1,7 @@
+export const MainContent = () => {
+    return(
+        <main>
+            <p>Main Content</p>
+        </main>
+    )
+}
